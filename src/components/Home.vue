f76166080c19b1d2e0d017a15eeba5ed7d9b7294
Bradley Rastrullo <brastrullo@gmail.com>
2020-10-18 09:06:26 -0400

    style: Update Header and h1 text

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,9 @@
 <template>
   <v-container>
     <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
 
       <v-col class="mb-4 mt-10">
-        <h1 class="display-2 font-weight-bold mb-3">Movies Fan</h1>
+        <h1 color="#0d253f" class="display-2 font-weight-bold mb-3">Movie Fan</h1>
 
         <p class="subheading font-weight-regular">Please complete profile</p>
       </v-col>
